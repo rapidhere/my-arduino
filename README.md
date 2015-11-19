c20f47378feaaf6fbe03baa43ddf2d2a2145b503
rapidhere <rapidhere@gmail.com>
2015-11-19 21:56:20 +0800

    add clang compelete

--- README.md
+++ README.md
@@ -9,4 +9,6 @@ Learning Arduino
 
 HOW TO BUILD
 ---
-change the build_flags in platformio.ini, change `-D` option to the build taget_name, usually the build target name macro is `_BUILD_target_name`
+设置`platformio.ini`中的build_flags设置， 重新设置`-D`选项为你的目标名称，通常这个名字是`_BUILD_<taget_name>`
+
+如果需要clang的补全，在`.clang_complete`做同样的更改
--- demo/blink.cpp
+++ demo/blink.cpp
@@ -5,14 +5,14 @@
 #include "framework.h"
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);     // set pin as output
+    pinMode(LED_PIN, OUTPUT);     // set pin as output
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);  // set the LED on
-  delay(1000);                  // wait for a second
-  digitalWrite(LED_PIN, LOW);   // set the LED off
-  delay(1000);                  // wait for a second
+    digitalWrite(LED_PIN, HIGH);  // set the LED on
+    delay(1000);                  // wait for a second
+    digitalWrite(LED_PIN, LOW);   // set the LED off
+    delay(1000);                  // wait for a second
 }
 
 #endif
